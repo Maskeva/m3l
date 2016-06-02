950ab0e36a1ba78f14c438239a75c0136144c10c
Maskeva <zhongguoyinhang_17@qq.com>
2016-06-02 15:10:24 +0800

    修改Dockerfile代码

--- CentOS7/shadowsocks/start.sh
+++ CentOS7/shadowsocks/start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 echo "--------------------------------------------------------"
-echo " server password: $SS_PASSWORD "
+echo "              server password: $SS_PASSWORD         "
 echo "              encryption method: $SS_METHOD         "
 echo "--------------------------------------------------------"
 
 
 ssserver -s 0.0.0.0 -p 9528 -k $SS_PASSWORD -m $SS_METHOD -t 300
+
 #ssserver -c /root/shadowsocks.json -d start
