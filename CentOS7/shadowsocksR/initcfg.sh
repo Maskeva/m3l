4a00ed9f24656c1380ea69136a3d553963823e8c
Maskeva <zhongguoyinhang_17@qq.com>
2017-01-05 19:39:35 +0800

    u
    u

--- CentOS7/shadowsocksR/initcfg.sh
+++ CentOS7/shadowsocksR/initcfg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod +x /SSR/*.sh
-chmod +x /SSR/shadowsocks/*.sh
-cp -n /SSR/apiconfig.py /SSR/userapiconfig.py
-cp -n /SSR/config.json /SSR/user-config.json
-cp -n /SSR/mysql.json /SSR/usermysql.json
+chmod +x /shadowsocksr/*.sh
+chmod +x /shadowsocksr/shadowsocks/*.sh
+cp -n /shadowsocksr/apiconfig.py /shadowsocksr/userapiconfig.py
+cp -n /shadowsocksr/config.json /shadowsocksr/user-config.json
+cp -n /shadowsocksr/mysql.json /shadowsocksr/usermysql.json
